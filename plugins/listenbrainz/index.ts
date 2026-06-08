7663e38ac74f7c20a6efd7c36efc91abb42cd333
Petr Mácha <info@mapetr.moe>
2026-06-08 13:00:11 +0200

    check if VencordNative actually exists

--- plugins/listenbrainz/index.ts
+++ plugins/listenbrainz/index.ts
@@ -18,8 +18,9 @@ let interval: number;
 
 export function onLoad() {
 	interval = setInterval(() => {getScrobble()}, 10000);
-
-	window.VencordNative.csp.requestAddOverride("https://shcors.uwu.network", ["connect-src"], "Profile Theming plugin");
+	if (window.VencordNative) {
+		window.VencordNative.csp.requestAddOverride("https://shcors.uwu.network", ["connect-src"], "Profile Theming plugin");
+	}
 }
 
 export function onUnload() {
