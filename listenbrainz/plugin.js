537d2bbc1d5fcca4aba156925a2889832b512161
Mapetr <Mapetr@users.noreply.github.com>
2026-06-08 11:00:30 +0000

    deploy: 7663e38ac74f7c20a6efd7c36efc91abb42cd333

--- listenbrainz/plugin.js
+++ listenbrainz/plugin.js
@@ -192,7 +192,7 @@ function onLoad() {
 	interval = setInterval(() => {
 		getScrobble();
 	}, 1e4);
-	window.VencordNative.csp.requestAddOverride("https://shcors.uwu.network", ["connect-src"], "Profile Theming plugin");
+	if (window.VencordNative) window.VencordNative.csp.requestAddOverride("https://shcors.uwu.network", ["connect-src"], "Profile Theming plugin");
 }
 function onUnload() {
 	clearInterval(interval);
